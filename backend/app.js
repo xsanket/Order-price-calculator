d5d82c7daaccf6938b1a6ee84087b9f66f2c95d3
xsanket <xsanketkamble232@gmail.com>
2024-03-31 11:18:16 +0530

    "deployment config"

--- backend/app.js
+++ backend/app.js
@@ -30,15 +30,24 @@ sequelize
 
 
   // deployment config
-const path = require("path");
-__dirname = path.resolve();
+  import express from 'express';
+  import path from 'path';
+  
+ 
+  const __dirname = path.resolve();
+  
+  if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, 'frontend', 'build')));
+  
+    app.get('*', (req, res) => {
+      res.sendFile(path.join(__dirname, 'frontend', 'build', 'index.html'));
+    });
+  }
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/frontend/build")));
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "frontend", "build", "index.html"));
+  
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
   });
-}
 
 
 //test data
